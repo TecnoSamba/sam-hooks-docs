5b21cccd8795e9015a37b1a18bf613d9a21fb121
Nicolás Santísima Trinidad Santín <sannicolas2121@outlook.com>
2025-05-30 16:23:42 +0200

    Creating the docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,27 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'SamHooks - Docs',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/tecnosamba21/sam-hooks' }, { icon: 'npm', label: 'npm', href: 'https://www.npmjs.com/package/@tecnosamba21/sam-hooks' }],
+        sidebar: [
+            {
+                label: 'SamHooks',
+                autogenerate: { directory: 'guides' }
+            },
+        ],
+        customCss: [
+            './src/styles/global.css'
+        ]
+    }), react()],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
